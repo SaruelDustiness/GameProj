ab13f0a4ef2b12dc06a648d60c373af55da83813
Samuel <alpacadechinelo@gmail.com>
2023-02-10 17:30:44 -0300

    Isso é... um ataque?
    Animação de ataque foi adicionada aos inimigos. Ela é ativada quando eles chegam na hitbox do jogador.

--- Zeldinha/src/com/newbie/entities/Enemy.java
+++ Zeldinha/src/com/newbie/entities/Enemy.java
@@ -11,9 +11,9 @@ import com.newbie.world.World;
 
 public class Enemy extends Entity{
 	
-	private int framesDmg = 0, framesRun = 0, framesAtk = 0, framesDie = 0;
-	private int maxFramesDmg = 6, maxFramesRun = 7, maxFramesAtk = 6, maxFramesDie = 7;
-	private int dmg = 0, maxDmg = 5, run = 0, maxRun = 6, atk = 0, maxAtk = 5, die = 0, maxDie = 6;
+	private int framesAtk = 0, framesRun = 0, framesIdle = 0, framesDie = 0;
+	private int maxFramesAtk = 6, maxFramesRun = 7, maxFramesIdle = 6, maxFramesDie = 7;
+	private int atk = 0, maxAtk = 5, run = 0, maxRun = 6, idle = 0, maxIdle = 5, die = 0, maxDie = 6;
 	private boolean last = true;
 	
 	private double speed = 1;
@@ -23,21 +23,21 @@ public class Enemy extends Entity{
 
 	private BufferedImage[] rightBlueRun;
 	private BufferedImage[] leftBlueRun;
-	private BufferedImage[] rightBlueAtk;
-	private BufferedImage[] leftBlueAtk;
+	private BufferedImage[] rightBlueIdle;
+	private BufferedImage[] leftBlueIdle;
 	private BufferedImage[] rightBlueDie;
 	private BufferedImage[] leftBlueDie;
-	private BufferedImage[] rightBlueDmg;
-	private BufferedImage[] leftBlueDmg;
+	private BufferedImage[] rightBlueAtk;
+	private BufferedImage[] leftBlueAtk;
 	
 	private BufferedImage[] rightBrownRun;
 	private BufferedImage[] leftBrownRun;
-	private BufferedImage[] rightBrownAtk;
-	private BufferedImage[] leftBrownAtk;
+	private BufferedImage[] rightBrownIdle;
+	private BufferedImage[] leftBrownIdle;
 	private BufferedImage[] rightBrownDie;
 	private BufferedImage[] leftBrownDie;
-	private BufferedImage[] rightBrownDmg;
-	private BufferedImage[] leftBrownDmg;
+	private BufferedImage[] rightBrownAtk;
+	private BufferedImage[] leftBrownAtk;
 	
 //	private int maskx = 8, masky = 8, maskw = 5, maskh = 5;
 
@@ -46,70 +46,70 @@ public class Enemy extends Entity{
 		this.blue = blue;
 		
 		//Cogumelo Azul
-		rightBlueAtk = new BufferedImage[6];
+		rightBlueIdle = new BufferedImage[6];
 		rightBlueRun = new BufferedImage[7];
-		rightBlueDmg = new BufferedImage[6];
+		rightBlueAtk = new BufferedImage[6];
 		rightBlueDie = new BufferedImage[7];
-		leftBlueAtk = new BufferedImage[6];
+		leftBlueIdle = new BufferedImage[6];
 		leftBlueRun = new BufferedImage[7];
-		leftBlueDmg = new BufferedImage[6];
+		leftBlueAtk = new BufferedImage[6];
 		leftBlueDie = new BufferedImage[7];
 		
 		for(int i = 0; i < 6; i++) {
-			rightBlueAtk[i] = Game.enemyOne.getSprite(0 + (i*16), 0, 16, 16);
+			rightBlueIdle[i] = Game.enemyOne.getSprite(0 + (i*16), 0, 16, 16);
 		}
 		for(int i = 0; i < 7; i++) {
 			rightBlueRun[i] = Game.enemyOne.getSprite(0 + (i*16), 16, 16, 16);
 		}
 		for(int i = 0; i < 6; i++) {
-			rightBlueDmg[i] = Game.enemyOne.getSprite(0 + (i*16), 32, 16, 16);
+			rightBlueAtk[i] = Game.enemyOne.getSprite(0 + (i*16), 32, 16, 16);
 		}
 		for(int i = 0; i < 7; i++) {
 			rightBlueDie[i] = Game.enemyOne.getSprite(0 + (i*16), 48, 16, 16);
 		}
 		for(int i = 0; i < 6; i++) {
-			leftBlueAtk[i] = Game.enemyOne.getSprite(0 + (i*16), 64, 16, 16);
+			leftBlueIdle[i] = Game.enemyOne.getSprite(0 + (i*16), 64, 16, 16);
 		}
 		for(int i = 0; i < 7; i++) {
 			leftBlueRun[i] = Game.enemyOne.getSprite(0 + (i*16), 80, 16, 16);
 		}
 		for(int i = 0; i < 6; i++) {
-			leftBlueDmg[i] = Game.enemyOne.getSprite(0 + (i*16), 96, 16, 16);
+			leftBlueAtk[i] = Game.enemyOne.getSprite(0 + (i*16), 96, 16, 16);
 		}
 		for(int i = 0; i < 7; i++) {
 			leftBlueDie[i] = Game.enemyOne.getSprite(0 + (i*16), 112, 16, 16);
 		}
 		
 		//Cogumelo Marrom
-		rightBrownAtk = new BufferedImage[6];
+		rightBrownIdle = new BufferedImage[6];
 		rightBrownRun = new BufferedImage[7];
-		rightBrownDmg = new BufferedImage[6];
+		rightBrownAtk = new BufferedImage[6];
 		rightBrownDie = new BufferedImage[7];
-		leftBrownAtk = new BufferedImage[6];
+		leftBrownIdle = new BufferedImage[6];
 		leftBrownRun = new BufferedImage[7];
-		leftBrownDmg = new BufferedImage[6];
+		leftBrownAtk = new BufferedImage[6];
 		leftBrownDie = new BufferedImage[7];
 		
 		for(int i = 0; i < 6; i++) {
-			rightBrownAtk[i] = Game.enemyTwo.getSprite(0 + (i*16), 0, 16, 16);
+			rightBrownIdle[i] = Game.enemyTwo.getSprite(0 + (i*16), 0, 16, 16);
 		}
 		for(int i = 0; i < 7; i++) {
 			rightBrownRun[i] = Game.enemyTwo.getSprite(0 + (i*16), 16, 16, 16);
 		}
 		for(int i = 0; i < 6; i++) {
-			rightBrownDmg[i] = Game.enemyTwo.getSprite(0 + (i*16), 32, 16, 16);
+			rightBrownAtk[i] = Game.enemyTwo.getSprite(0 + (i*16), 32, 16, 16);
 		}
 		for(int i = 0; i < 7; i++) {
 			rightBrownDie[i] = Game.enemyTwo.getSprite(0 + (i*16), 48, 16, 16);
 		}
 		for(int i = 0; i < 6; i++) {
-			leftBrownAtk[i] = Game.enemyTwo.getSprite(0 + (i*16), 64, 16, 16);
+			leftBrownIdle[i] = Game.enemyTwo.getSprite(0 + (i*16), 64, 16, 16);
 		}
 		for(int i = 0; i < 7; i++) {
 			leftBrownRun[i] = Game.enemyTwo.getSprite(0 + (i*16), 80, 16, 16);
 		}
 		for(int i = 0; i < 6; i++) {
-			leftBrownDmg[i] = Game.enemyTwo.getSprite(0 + (i*16), 96, 16, 16);
+			leftBrownAtk[i] = Game.enemyTwo.getSprite(0 + (i*16), 96, 16, 16);
 		}
 		for(int i = 0; i < 7; i++) {
 			leftBrownDie[i] = Game.enemyTwo.getSprite(0 + (i*16), 112, 16, 16);
@@ -128,35 +128,58 @@ public class Enemy extends Entity{
 			}
 		}
 		
-		if(Game.rand.nextInt(100) < 20) {
-			
-			if(((int)x < Game.player.getX()) && (World.isFree((int)(this.getX()+speed), this.getY())) 
-					&& !isColidding((int)(this.getX()+speed), this.getY())) {
-				x+=speed;
-				dir = right_dir;
-				last = true;
-			}else if(((int)x > Game.player.getX()) && (World.isFree((int)(this.getX()-speed), this.getY()))
-					&& !isColidding((int)(this.getX()-speed), this.getY())) {
-				x-=speed;
-				dir = left_dir;
-				last = false;
+		if(isCollidingWithPlayer()) {
+			framesAtk++;
+			if(framesAtk == maxFramesAtk) {
+				framesAtk = 0;
+				atk++;
+				if(atk > maxAtk) {
+					atk = 0;
+				}
 			}
+		}
+		if(!isCollidingWithPlayer()) {
 			
-			if(((int)y < Game.player.getY()) && (World.isFree(this.getX(), (int)(this.getY()+speed)))
-					&& !isColidding(this.getX(), (int)(this.getY()+speed))) {
-				y+=speed;
-				dir = up_dir;
-			}else if(((int)y > Game.player.getY()) && (World.isFree(this.getX(), (int)(this.getY()-speed)))
-					&& !isColidding(this.getX(), (int)(this.getY()-speed)))	{
-				y-=speed;
-				dir = down_dir;
+			if(Game.rand.nextInt(100) < 20) {
+				
+				if(((int)x < Game.player.getX()) && (World.isFree((int)(this.getX()+speed), this.getY())) 
+						&& !isColliding((int)(this.getX()+speed), this.getY())) {
+					x+=speed;
+					dir = right_dir;
+					last = true;
+				}else if(((int)x > Game.player.getX()) && (World.isFree((int)(this.getX()-speed), this.getY()))
+						&& !isColliding((int)(this.getX()-speed), this.getY())) {
+					x-=speed;
+					dir = left_dir;
+					last = false;
+				}
+				
+				if(((int)y < Game.player.getY()) && (World.isFree(this.getX(), (int)(this.getY()+speed)))
+						&& !isColliding(this.getX(), (int)(this.getY()+speed))) {
+					y+=speed;
+					dir = up_dir;
+				}else if(((int)y > Game.player.getY()) && (World.isFree(this.getX(), (int)(this.getY()-speed)))
+						&& !isColliding(this.getX(), (int)(this.getY()-speed)))	{
+					y-=speed;
+					dir = down_dir;
+				}
+				
 			}
 			
 		}
 		
 	}
 	
-	public boolean isColidding(int xNext, int yNext) {
+	public boolean isCollidingWithPlayer() {
+		
+		Rectangle enemyCurrent = new Rectangle(this.getX(), this.getY(), World.TILE_SIZE, World.TILE_SIZE);
+		Rectangle player = new Rectangle(Game.player.getX(), Game.player.getY(), 24, 24);
+		
+		return enemyCurrent.intersects(player);
+		
+	}
+	
+	public boolean isColliding(int xNext, int yNext) {
 		
 		Rectangle enemyCurrent = new Rectangle(xNext, yNext, World.TILE_SIZE, World.TILE_SIZE);
 		
@@ -184,46 +207,71 @@ public class Enemy extends Entity{
 	
 	public void render(Graphics g) {
 		
-		if(blue) {
+		if(!isCollidingWithPlayer()) { // Inimigo Correndo
+		
+			if(blue) { 
+				
+				if(dir == right_dir) {
+					g.drawImage(rightBlueRun[run], this.getX() - Camera.x, this.getY() - Camera.y, null);
+				}
+				else if(dir == left_dir) {
+					g.drawImage(leftBlueRun[run], this.getX() - Camera.x, this.getY() - Camera.y, null);
+				}
+				if((dir == up_dir) && last) {
+					g.drawImage(rightBlueRun[run], this.getX() - Camera.x, this.getY() - Camera.y, null);
+				}
+				else if((dir == down_dir) && !last) {
+					g.drawImage(leftBlueRun[run], this.getX() - Camera.x, this.getY() - Camera.y, null);
+				}
+				if((dir == up_dir) && !last) {
+					g.drawImage(leftBlueRun[run], this.getX() - Camera.x, this.getY() - Camera.y, null);
+				}
+				else if((dir == down_dir) && last) {
+					g.drawImage(rightBlueRun[run], this.getX() - Camera.x, this.getY() - Camera.y, null);
+				}
+				
+			}else {
+				if(dir == right_dir) {
+					g.drawImage(rightBrownRun[run], this.getX() - Camera.x, this.getY() - Camera.y, null);
+				}
+				else if(dir == left_dir) {
+					g.drawImage(leftBrownRun[run], this.getX() - Camera.x, this.getY() - Camera.y, null);
+				}
+				if((dir == up_dir) && last) {
+					g.drawImage(rightBrownRun[run], this.getX() - Camera.x, this.getY() - Camera.y, null);
+				}
+				else if((dir == down_dir) && !last) {
+					g.drawImage(leftBrownRun[run], this.getX() - Camera.x, this.getY() - Camera.y, null);
+				}
+				if((dir == up_dir) && !last) {
+					g.drawImage(leftBrownRun[run], this.getX() - Camera.x, this.getY() - Camera.y, null);
+				}
+				else if((dir == down_dir) && last) {
+					g.drawImage(rightBrownRun[run], this.getX() - Camera.x, this.getY() - Camera.y, null);
+				}
+			}
 			
-			if(dir == right_dir) {
-				g.drawImage(rightBlueRun[run], this.getX() - Camera.x, this.getY() - Camera.y, null);
-			}
-			else if(dir == left_dir) {
-				g.drawImage(leftBlueRun[run], this.getX() - Camera.x, this.getY() - Camera.y, null);
-			}
-			if((dir == up_dir) && last) {
-				g.drawImage(rightBlueRun[run], this.getX() - Camera.x, this.getY() - Camera.y, null);
-			}
-			else if((dir == down_dir) && !last) {
-				g.drawImage(leftBlueRun[run], this.getX() - Camera.x, this.getY() - Camera.y, null);
-			}
-			if((dir == up_dir) && !last) {
-				g.drawImage(leftBlueRun[run], this.getX() - Camera.x, this.getY() - Camera.y, null);
-			}
-			else if((dir == down_dir) && last) {
-				g.drawImage(rightBlueRun[run], this.getX() - Camera.x, this.getY() - Camera.y, null);
-			}
+		}else { // Inimigo atacando!
 			
-		}else {
-			if(dir == right_dir) {
-				g.drawImage(rightBrownRun[run], this.getX() - Camera.x, this.getY() - Camera.y, null);
+			if(blue) {
+				
+				if(last) {
+					g.drawImage(rightBlueAtk[atk], this.getX() - Camera.x, this.getY() - Camera.y, null);
+				}
+				else if(!last) {
+					g.drawImage(leftBlueAtk[atk], this.getX() - Camera.x, this.getY() - Camera.y, null);
+				}
+			
+			}else {
+				if(last) {
+					g.drawImage(rightBrownAtk[atk], this.getX() - Camera.x, this.getY() - Camera.y, null);
+				}
+				else if(!last) {
+					g.drawImage(leftBrownAtk[atk], this.getX() - Camera.x, this.getY() - Camera.y, null);
+				}
+			
 			}
-			else if(dir == left_dir) {
-				g.drawImage(leftBrownRun[run], this.getX() - Camera.x, this.getY() - Camera.y, null);
-			}
-			if((dir == up_dir) && last) {
-				g.drawImage(rightBrownRun[run], this.getX() - Camera.x, this.getY() - Camera.y, null);
-			}
-			else if((dir == down_dir) && !last) {
-				g.drawImage(leftBrownRun[run], this.getX() - Camera.x, this.getY() - Camera.y, null);
-			}
-			if((dir == up_dir) && !last) {
-				g.drawImage(leftBrownRun[run], this.getX() - Camera.x, this.getY() - Camera.y, null);
-			}
-			else if((dir == down_dir) && last) {
-				g.drawImage(rightBrownRun[run], this.getX() - Camera.x, this.getY() - Camera.y, null);
-			}
+				
 		}
 
 //		super.render(g);
